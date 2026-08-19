edc11c1e01a13f8786d94e40af635dfd5da675a9
josé <josehneri1998@outlook.com>
2026-08-18 23:48:28 -0300

    DIC038 - Corrige separação das operações de produto

--- principal.py
+++ principal.py
@@ -1,29 +1,81 @@
-def cadastro(cadastro_produtos):
-          while True:       
-               produto=validacoes.ler_produto(cadastro_produtos)  
-               preço=validacoes.ler_preço()
-               quantidade=validacoes.ler_quantidade()
-          
-               dados={
-                      "preço":preço,
-                      "quantidade": quantidade}
-    
-               cadastro_produtos[produto]=dados 
-               persistencia.salvar_json(cadastro_produtos)
-               while True:
-                    print("deseja continuar ? sim/nao")
-                    continuar=input()
-                    if continuar=="nao":
-                       sair=True
-                       break
-                    elif continuar=="sim":
-                        sair=False 
-                        break                
-             
-                    else:
-                      print("opçao invalida!")  
-    
-          
+import produtos
+import relatorios
+import persistencia
 
-               if sair==True:
-                  return
+cadastro_produtos=persistencia.carregar_json()
+
+
+
+
+def menu(cadastro_produtos):
+  
+  while True:
+        print("1 - Cadastrar produto")
+        print("2 - Consultar produto")
+        print("3 - Alterar produto")
+        print("4 - excluir produto")
+        print("5 - Gerar relatorio")
+        print("6 - Ler relatorio")
+        print("7 - Salvar Json")
+        print("8 - Encerrar")
+        try:
+           opcao=int(input())
+           if opcao<9 and opcao>0:
+              return opcao
+             
+           else:
+               print("opcao inexistente")
+        except ValueError:
+             print("seu animal, digite UM NUMERO")
+             continue    
+
+        while True:      
+     
+            opcao=menu()       
+    
+            if opcao==1:
+              produtos.cadastro(cadastro_produtos)
+   
+            elif opcao==2:
+                produtos.consultar_produto(cadastro_produtos)
+    
+            elif opcao==3:
+                produtos.Alteração_produto(cadastro_produtos)
+    
+            elif opcao==4:
+                produtos.excluir_produto(cadastro_produtos)
+    
+            elif opcao==5:
+                relatorios.gerar_relatorio(cadastro_produtos)
+          
+            elif opcao==6:
+                relatorios.ler_relatorio() 
+
+            elif opcao==7:
+                persistencia.salvar_json(cadastro_produtos)
+
+     
+
+            elif opcao==8:
+                 while True:
+                     print("Tem certeza que deseja finalizar ?") 
+                     resposta=input()
+                     if resposta =="sim":
+                         encerrar=True
+                         break
+         
+            elif resposta=="nao":
+                  print("voltando ao menu")
+                  break
+                    
+            else:
+                print("resposta invalida")
+        
+            if encerrar==True:
+               print("programa finalizado")
+               break
+
+
+
+
+menu(cadastro_produtos)
--- produtos.py
+++ produtos.py
@@ -1,9 +1,8 @@
 
 
 import persistencia
-import relatorios
 import validacoes
-cadastro_produtos=persistencia.carregar_json()
+
 encerrar=False
 
 
@@ -97,7 +96,7 @@ def Alteração_produto(cadastro_produtos):
 def excluir_produto(cadastro_produtos):
     print("qual produto deseja excluir?")
     produto=input()
-    dados=validacoes.buscar_produto(produto,)
+    dados=validacoes.buscar_produto(produto,cadastro_produtos)
     if dados:
        print(dados)
        print("tem certeza que deseja excluir ?")
@@ -117,82 +116,3 @@ def excluir_produto(cadastro_produtos):
     else:
        print("produto nao cadastrado")
 
-
-
-
-
-
-
-
-          
-                 
-          
-          
-
-
-def menu():
-  while True:
-        print("1 - Cadastrar produto")
-        print("2 - Consultar produto")
-        print("3 - Alterar produto")
-        print("4 - excluir produto")
-        print("5 - Gerar relatorio")
-        print("6 - Ler relatorio")
-        print("7 - Salvar Json")
-        print("8 - Encerrar")
-        try:
-           opcao=int(input())
-           if opcao<9 and opcao>0:
-              return opcao
-             
-           else:
-               print("opcao inexistente")
-        except ValueError:
-             print("seu animal, digite UM NUMERO")
-             continue
-
-        
-while True: 
-     opcao=menu()       
-    
-     if opcao==1:
-        cadastro(cadastro_produtos)
-   
-     elif opcao==2:
-          consultar_produto(cadastro_produtos)
-    
-     elif opcao==3:
-          Alteração_produto(cadastro_produtos)
-    
-     elif opcao==4:
-          excluir_produto()
-    
-     elif opcao==5:
-          relatorios.gerar_relatorio(cadastro_produtos)
-          
-     elif opcao==6:
-          relatorios.ler_relatorio() 
-
-     elif opcao==7:
-          persistencia.salvar_json(cadastro_produtos)
-
-     
-
-     elif opcao==8:
-          while True:
-              print("Tem certeza que deseja finalizar ?") 
-              resposta=input()
-              if resposta =="sim":
-                 encerrar=True
-                 break
-         
-              elif resposta=="nao":
-                   print("voltando ao menu")
-                   break
-                    
-              else:
-                 print("resposta invalida")
-        
-     if encerrar==True:
-        print("programa finalizado")
-        break
